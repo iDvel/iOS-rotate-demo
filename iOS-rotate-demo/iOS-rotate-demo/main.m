5b0ef8382f20f245aab23558991a0a61b641e09b
iDvel <paldvel@gmail.com>
2016-04-20 02:12:27 +0800

    first commit

---
+++ iOS-rotate-demo/iOS-rotate-demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS-rotate-demo
+//
+//  Created by Dvel on 16/4/20.
+//  Copyright © 2016年 Dvel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
